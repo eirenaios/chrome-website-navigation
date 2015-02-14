6c8b0cec14a270f42f7c44afe0c9eedc048f3b57
eirenaios <suresh.onezero@gmail.com>
2015-02-14 19:00:41 +0530

    Simple navigation completed. Mega menu can be added into this.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--This project shows google plus navigation,my one of the favourite navigation menu.-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Google Chrome Navigation Menu</title>
+        <link href="assets/css/normalize.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/nav.css" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,600,400' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <nav>
+            <ul class="menu">
+                <li><a href="#">Download</a></li>
+                <li><a href="#">Setup</a></li>
+                <li><a href="#">Chromebook</a></li>
+                <li><a href="#">Chromecast</a>
+                    <ul class="submenu">
+                        <li><a href="#">Setup</a></li>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Buy nline</a></li>
+                        <li><a href="#">Buy in store</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </body>
+</html>
